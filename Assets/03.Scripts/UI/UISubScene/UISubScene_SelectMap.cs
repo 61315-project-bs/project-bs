2e621b0446c273e4cc4a6158bcc6a800801de1c0
ColeYoung <ighong11@gmail.com>
2024-10-03 12:02:23 +0900

    [Add] ui set level funcition added.
    - 난이도 설정 로직 구현

--- Assets/03.Scripts/UI/UISubScene/UISubScene_SelectMap.cs
+++ Assets/03.Scripts/UI/UISubScene/UISubScene_SelectMap.cs
@@ -1,30 +1,60 @@
-using UnityEngine;
+﻿using UnityEngine;
 using UnityEngine.UI;
 
 public class UISubScene_SelectMap : UISubScene
 {
-  [SerializeField] Button OpenTestOverlapSubSceneButton;
-  private UISubScene_SelectMap_Presenter presenter;
+    [SerializeField] Button OpenTestOverlapSubSceneButton;
+    [Tooltip("0 : left button \n 1 : right button")]
+    [SerializeField] private Button[] _btnsSetLevel;
+    [SerializeField] private Text _txtSetLevel;
+    [SerializeField] private Button _decision;
+    private UISubScene_SelectMap_Presenter presenter;
+    
 
-  protected override void Awake()
-  {
-    base.Awake();
-    presenter = new UISubScene_SelectMap_Presenter(this);
-    InitializeButtons();
-  }
+    protected override void Awake()
+    {
+        base.Awake();
+        presenter = new UISubScene_SelectMap_Presenter(this);
+        presenter.Init(_btnsSetLevel);
+        InitializeButtons();
+    }
 
-  private void InitializeButtons()
-  {
-    OpenTestOverlapSubSceneButton.onClick.AddListener(presenter.OpenTestOverlapSubScene);
-  }
+    private void InitializeButtons()
+    {
+        OpenTestOverlapSubSceneButton.onClick.AddListener(presenter.OpenTestOverlapSubScene);
+        _btnsSetLevel[0].onClick.AddListener(() => presenter.SetLevel(_txtSetLevel, true));
+        _btnsSetLevel[1].onClick.AddListener(() => presenter.SetLevel(_txtSetLevel, false));
+        //_decision.onClick.AddListener(() => UIManager.Instance.InstantiateUIPopup<UIPopup>("ss"));
+    }
 }
 
 public class UISubScene_SelectMap_Presenter : Presenter
 {
-  public UISubScene_SelectMap_Presenter(UIBase view) : base(view) { }
+    public UISubScene_SelectMap_Presenter(UIBase view) : base(view) { }
 
-  public void OpenTestOverlapSubScene()
-  {
-    UIManager.Instance.InstantiateUISubScene<UISubScene_Test>();
-  }
+    /// <summary>
+    /// 좋은 방법은 아닌듯..일단 임시로 구현
+    /// </summary>
+    private Button[] _btnSetLevel;
+
+    public void Init(Button[] btnSetLevel)
+    {
+        _btnSetLevel = btnSetLevel;
+    }
+    public void SetLevel(Text text, bool isLeft)
+    {
+        if (isLeft)
+            TempMapDataHandler.Instance.CurrLevel--;
+        else
+            TempMapDataHandler.Instance.CurrLevel++;
+
+        _btnSetLevel[0].interactable = TempMapDataHandler.Instance.CurrLevel != 0;
+        _btnSetLevel[1].interactable = TempMapDataHandler.Instance.CurrLevel != TempMapDataHandler.Instance.MaxLevel;
+
+        text.text = TempMapDataHandler.Instance.CurrLevelStr;
+    }
+    public void OpenTestOverlapSubScene()
+    {
+        UIManager.Instance.InstantiateUISubScene<UISubScene_Test>();
+    }
 }
