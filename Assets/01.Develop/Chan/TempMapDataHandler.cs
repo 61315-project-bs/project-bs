1ea221dcc22c84c3ca35ac752ba44a4da916c4aa
ColeYoung <96222069+coleyoung-game@users.noreply.github.com>
2024-10-03 16:56:47 +0900

    Feature/UI select map (#3)
    * [Add] add UI_SelectMap scene.
    
    * [Add] Temp Model class added.
    
    - 임시로 모델을 담는 데이터 핸들러 추가
    
    * [Add] ui set level funcition added.
    
    - 난이도 설정 로직 구현
    
    * [Add] select Map logic added.
    
    - resources 파일에 Art/Sprite 추가
    - 맵 선택에 대한 전체적인 로직 구현 완료
    - 임시 2D Outline 쉐이더 추가(실사용 X)

---
+++ Assets/01.Develop/Chan/TempMapDataHandler.cs
@@ -0,0 +1,99 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+[Serializable]
+public class Map
+{
+    [SerializeField] private string _name;
+    [SerializeField] private string _desc;
+    [SerializeField] private Sprite _mapIcon;
+    [SerializeField] private bool _isLock;
+    public Sprite Icon 
+    { 
+        get 
+        {
+            if (_isLock)
+                return ResourceManager.Instance.Load<Sprite>("Art/Sprite/Img_Lock");
+            else
+                return _mapIcon; 
+        } 
+    }
+    public string Name 
+    { 
+        get 
+        {
+            if (_isLock)
+                return "???";
+            else
+                return _name; 
+        } 
+    }
+    public string Desc 
+    { 
+        get 
+        {
+            if (_isLock)
+                return "아직 확인되지 않은 지역입니다.";
+            else
+                return _desc; 
+        } 
+    }
+    public bool IsLock { get { return _isLock; } }
+}
+
+
+
+public class TempMapDataHandler : Singleton<TempMapDataHandler>
+{
+    #region Map
+    [SerializeField] private Map[] _maps;
+    private int _selectMapIdx = -1;
+    public Map[] Maps { get { return _maps; } }
+    /// <summary>
+    /// -1은 맵을 선택하지 않은 상태.
+    /// </summary>
+    public int SelectMapIdx
+    {
+        get { return _selectMapIdx; }
+        set
+        {
+            if (value > _maps.Length - 1)
+                Debug.LogError($"[TempMapDataHandler]::::CurrMapIdx is invalid. -> {value}");
+            else
+                _selectMapIdx = value;
+        }
+    }
+    #endregion~Map
+    #region SetLevel
+    private readonly string[] STR_LEVEL = { "easy", "medium", "hard" };
+    private int _maxLevel; 
+    private int _currLevel = 0;
+    
+    public string CurrLevelStr { get { return STR_LEVEL[_currLevel]; } }
+    public int MaxLevel { get { return _maxLevel; } }
+    public int CurrLevel { get { return _currLevel; } 
+        set
+        {
+            // 0~2 범위에서만 설정할 수 있게 적용
+            if (value > _maxLevel)
+                _currLevel = _maxLevel;
+            else if (value < 0)
+                _currLevel = 0;
+            else
+                _currLevel = value;
+        }
+    }
+    #endregion~SetLevel
+
+    private void Start()
+    {
+        Init();
+    }
+    private void Init()
+    {
+        _maxLevel = STR_LEVEL.Length - 1;
+    }
+    
+}
--- Assets/03.Scripts/UI/UISubItem/UISubItem_Map.cs
+++ Assets/03.Scripts/UI/UISubItem/UISubItem_Map.cs
@@ -1,13 +1,76 @@
+using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.EventSystems;
 
-public class UISubItem_Map : UISubItem
+public class UISubItem_Map : UISubItem, IPointerEnterHandler, IPointerExitHandler
 {
-  protected override void Awake()
-  {
-    base.Awake();
-    GetComponent<Button>().onClick.AddListener(() =>
+    [SerializeField] private Button _btnSelect;
+    [SerializeField] private Image _imgMapIcon;
+    [SerializeField] private int _mapId;
+    [SerializeField] private UISubItem_MapDetail _mapDetail;
+    private UISubItem_Map_Presenter _presenter;
+    protected override void Awake()
     {
-      UIManager.Instance.InstantiateUISubScene<UISubScene_Test>();
-    });
-  }
+        base.Awake();
+        _presenter = new UISubItem_Map_Presenter(this);
+        _presenter.Init(_mapDetail,_imgMapIcon, _btnSelect, _mapId);
+        _btnSelect.onClick.AddListener(() =>
+        {
+            //UIManager.Instance.InstantiateUISubScene<UISubScene_Test>();
+            _presenter.OnSelectMap();
+        });
+    }
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        _presenter.OnMouseOver(true);
+    }
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        _presenter.OnMouseOver(false);
+    }
+}
+
+public class UISubItem_Map_Presenter : Presenter
+{
+    public UISubItem_Map_Presenter(UIBase view) : base(view) {}
+    private Map _mapData;
+    private GameObject _mapDetailObj;
+    private Image _img;
+    private int _idx;
+    private bool _isSelect = false;
+    public void Init(UISubItem_MapDetail mapDetail, Image mapIcon, Button btnSelect, int idx)
+    {
+        _idx = idx;
+        _mapData = TempMapDataHandler.Instance.Maps[_idx];
+        if(_mapData == null)
+        {
+            Debug.LogError($"[UISubItem_Map_Presenter]::::_mapData is null.");
+            return;
+        }
+        if (_mapData.IsLock) btnSelect.interactable = false;
+        mapIcon.sprite = _mapData.Icon;
+        _img = mapIcon;
+        SetMapDetail(mapDetail);
+    }
+    private void SetMapDetail(UISubItem_MapDetail mapDetail)
+    {
+        mapDetail.SetData(_mapData);
+        _mapDetailObj = mapDetail.gameObject;
+        _mapDetailObj.SetActive(false);
+    }
+    public void OnMouseOver(bool isOver)
+    {
+        _mapDetailObj.SetActive(isOver);
+    }
+    public void OnSelectMap()
+    {
+        if (_mapData.IsLock)
+            return;
+        _isSelect = !_isSelect;
+        TempMapDataHandler.Instance.SelectMapIdx = _isSelect ? _idx : -1;
+        Material mat = new Material(_img.material);
+        _img.material = mat;
+        mat.SetFloat("_IsOutline", _isSelect ? 1 : 0);
+    }
 }
