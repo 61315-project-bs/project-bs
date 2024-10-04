57641ef3a34a49ddbe4fe03e8a65b80f99663766
ColeYoung <ighong11@gmail.com>
2024-10-04 14:04:38 +0900

    [Update] dispose logic added in Presenter class.

--- Assets/03.Scripts/UI/Presenter.cs
+++ Assets/03.Scripts/UI/Presenter.cs
@@ -1,8 +1,18 @@
+using System.Collections.Generic;
+using System;
+
 public class Presenter<T> where T : UIBase
 {
-  protected T _view;
-  public Presenter(T view)
-  {
-    _view = view;
-  }
+    protected T _view;
+    protected Queue<IDisposable> _disposables;
+    public Presenter(T view)
+    {
+        _view = view;
+        _disposables = new Queue<IDisposable>();
+    }
+    public void DisopseStreams()
+    {
+        while (_disposables.Count > 0)
+            _disposables.Dequeue().Dispose();
+    }
 }
