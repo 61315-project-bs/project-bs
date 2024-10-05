cea0c876226b0fcae955c386a5ed45796bc603fb
ColeYoung <ighong11@gmail.com>
2024-10-05 16:07:38 +0900

    [Update] Change the way view is delivered.
    - Component 전달 -> Stream 전달

--- Assets/03.Scripts/UI/UISubScene/UISubScene_SelectMap.cs
+++ Assets/03.Scripts/UI/UISubScene/UISubScene_SelectMap.cs
@@ -12,10 +12,13 @@ public class UISubScene_SelectMap : UISubScene
     [SerializeField] private Text _txtSetLevel;
     [SerializeField] private Button _decision;
     private UISubScene_SelectMap_Presenter presenter;
-    public Button[] BtnSetLevel { get { return _btnsSetLevel; } }
-    public Button BtnDecision { get { return _decision; } }
-    public Button OpenTestOverlap { get { return  OpenTestOverlapSubSceneButton; } }
     public Text TxtSetLevel { get { return _txtSetLevel; } }
+
+    // Component 자체를 전달하는 것 보단, stream을 전달하는것이 추후 코드를 볼 때 명확하게 이해할 수 있을 것 같아서 수정 했습니다.
+    public IObservable<Unit> OnClick_SetLevel_Left { get { return _btnsSetLevel[0].OnClickAsObservable(); } }
+    public IObservable<Unit> OnClick_SetLevel_Right { get { return _btnsSetLevel[1].OnClickAsObservable(); } }
+    public IObservable<Unit> OnClick_Decision { get { return _decision.OnClickAsObservable(); } }
+    public IObservable<Unit> OnClick_OpenTestOverlap { get { return  OpenTestOverlapSubSceneButton.OnClickAsObservable(); } }
 
     protected override void Awake()
     {
@@ -28,6 +31,11 @@ public class UISubScene_SelectMap : UISubScene
         presenter.DisopseStreams();
         presenter = null;
     }
+    public void SetLevelActivater(bool isLeft, bool isRight)
+    {
+        _btnsSetLevel[0].interactable = isLeft;
+        _btnsSetLevel[1].interactable = isRight;
+    }
 }
 
 public class UISubScene_SelectMap_Presenter : Presenter<UISubScene_SelectMap>
@@ -36,30 +44,29 @@ public class UISubScene_SelectMap_Presenter : Presenter<UISubScene_SelectMap>
     {
         TempMapDataHandler.Instance.ReactCurrLevel
         .AsObservable()
-        .Subscribe(level => 
+        .Subscribe(level=>
         {
             if (level < 0)
                 level = 0;
             if (level > TempMapDataHandler.Instance.MaxLevel)
                 level = TempMapDataHandler.Instance.MaxLevel;
-            view.BtnSetLevel[0].interactable = level != 0;
-            view.BtnSetLevel[1].interactable = level != TempMapDataHandler.Instance.MaxLevel;
+            view.SetLevelActivater(level != 0, level != TempMapDataHandler.Instance.MaxLevel);
             view.TxtSetLevel.text = TempMapDataHandler.Instance.STR_LEVEL[level];
         }).EnqueueDispose(_disposables);
 
-        view.BtnSetLevel[0].OnClickAsObservable()
+        view.OnClick_SetLevel_Left
         .Subscribe(_ =>
         {
             TempMapDataHandler.Instance.CurrLevel--;
         }).EnqueueDispose(_disposables);
 
-        view.BtnSetLevel[1].OnClickAsObservable()
+        view.OnClick_SetLevel_Right
         .Subscribe(_ =>
         {
             TempMapDataHandler.Instance.CurrLevel++;
         }).EnqueueDispose(_disposables);
 
-        view.OpenTestOverlap.OnClickAsObservable()
+        view.OnClick_OpenTestOverlap
         .Subscribe(_ =>
         {
             UIManager.Instance.InstantiateUISubScene<UISubScene_Test>();
