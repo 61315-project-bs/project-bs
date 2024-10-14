e91a60f941f204d4f6da746d3ebd49c3b7d314fe
l11040 <l1104033837@gmail.com>
2024-10-14 17:23:27 +0900

    [Add] 데이터 매니저 구성 및 데이터 파싱 예시 코드 작성 (TestData)

---
+++ Assets/03.Scripts/Managers/Data/DefaultData.cs
@@ -0,0 +1,29 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// 딕셔너리로 만들어진 CSV 데이터들을 읽기 전용으로 관리하는 클래스
+/// </summary>
+public class DefaultData
+{
+  public Dictionary<string, TestData> TestData { get; private set; }
+
+  public void Init()
+  {
+    LoadData();
+  }
+
+  private void LoadData()
+  {
+    try
+    {
+      // ----- CSV 데이터 로드 시작 -----
+      TestData = DefaultDataLoader.ParseTestData();
+    }
+    catch (System.Exception ex)
+    {
+      string sceneName = UnityEngine.SceneManagement.SceneManager.GetActiveScene().name;
+      Debug.LogError($"{ex.Message}, {ex.StackTrace}, {sceneName}");
+    }
+  }
+}
